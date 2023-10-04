98af3ca260065e9a2be55b0d549398e5be88abea
Sollorix <142428468+Sollorix@users.noreply.github.com>
2023-10-04 08:15:05 -0500

    Added comments
    Added comments.

--- LINQ_Babcock_Olivia/Game.cs
+++ LINQ_Babcock_Olivia/Game.cs
@@ -8,14 +8,17 @@ namespace LINQ_Babcock_Olivia
 {
     internal class Game
     {
+        // Creates string properties and defines get value.
         public string Title { get; }
 
         public char Esrb { get; }
 
         public string Genre { get; }
 
+        // Creates and sets string parameters.
         public Game(string titleParam, char esrbParam, string genreParam)
         {
+            // Assigns each parameter to the corresponding property.
             Title = titleParam;
             Esrb = esrbParam;
             Genre = genreParam;
--- LINQ_Babcock_Olivia/Program.cs
+++ LINQ_Babcock_Olivia/Program.cs
@@ -7,8 +7,10 @@ namespace LINQ_Babcock_Olivia
     {
         static void Main(string[] args)
         {
+            // Creates an array for game.
             Game[] games = new Game[]
             {
+                // Lists all games and their properties.
                 new Game("SA2", 'E', "Racing"),
                 new Game("Sonic CD", 'E', "Platformer"),
                 new Game("MKX", 'M', "Fighting"),
@@ -21,37 +23,51 @@ namespace LINQ_Babcock_Olivia
                 new Game("MK11", 'M', "Fighting")
             };
 
+            // Creates a LINQ Query to get all games with 9 letters or less and assings it to a var.
             var shortGames = from game in games
                              where game.Title.Length < 9
+                             // Prints game title(s) with an uppercase letter
                              select $"Game Title: {game.Title.ToUpper()}";
 
+            // Sets a foreach loop for shortGames.
             foreach (var game in shortGames)
             {
+                // Prints game(s) to the console.
                 Console.WriteLine(game);
             }
 
+            // Creates a LINQ Query to find Minecraft in the array and assign a var to it.
             var mineCraft = games.Where(game => game.Title == "Minecraft")
                             .Select(game => $"Title: {game.Title}, ESRB: {game.Esrb}, Genre {game.Genre}");
 
+            // Prints all of the game properties of Minecraft.
             Console.WriteLine(mineCraft.FirstOrDefault());
 
+            // Creats a LINQ Query to find T rated games in the array and assign a var to it.
             var tRated = from game in games
                          where game.Esrb == 'T'
                          select game.Title;
 
+            // Prints to the console.
             Console.WriteLine("T Rated Games:");
+            // Creates a foreach loop for tRated.
             foreach (var game in tRated)
             {
+                // Prints game(s) to the console.
                 Console.WriteLine(game);
             }
 
+            // Creats a LINQ Query to find E rated action games in the array and assign a var to it.
             var eRatedAction = from game in games
                                where game.Esrb == 'E' && game.Genre.Contains("Action")
                                select game.Title;
 
+            // Prints to the console.
             Console.WriteLine("E Rated Action Games:");
+            // Creates a foreach loop for eRatedAction
             foreach (var game in eRatedAction)
             {
+                // Prints game(s) to the console.
                 Console.WriteLine(game);
             }
         }
